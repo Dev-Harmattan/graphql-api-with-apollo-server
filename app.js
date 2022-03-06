34b85292a5e896f2837ca20f9a857706181d4d36
Sodiq Olalekan <ganiyusodiq059@gmail.com>
2022-03-06 13:25:24 -0700

    clean error message

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const {ApolloServer, gql} = require('apollo-server');
+const {ApolloServer, gql, ApolloError} = require('apollo-server');
 const sessionData = require('./data/sessions.json');
 const SessionApi = require('./dataSource/session')
 const SpeakerApi = require('./datasource/speaker');
@@ -15,5 +15,15 @@ const dataSources = () => ({
 });
 
 
-const server = new ApolloServer({typeDefs, resolvers, dataSources});
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers, 
+  dataSources, 
+  debug: false,
+  formatError: (error) => {
+    if(error.extensions.code === "INTERNAL_SERVER_ERROR"){
+      return new ApolloError("We are having some trouble", "ERROR", {token: "uniquetoken"})
+    }
+  }
+});
 server.listen({port: process.env.PORT || 4000}).then(({url}) => console.log(`server is running on ${url}`))
