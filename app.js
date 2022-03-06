5a2013ef1e3caffe41d069a199e3fbed02f87624
Sodiq Olalekan <ganiyusodiq059@gmail.com>
2022-03-06 06:47:53 -0700

    schema created

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const {ApolloServer} = require('apollo-server');
+const {ApolloServer, gql} = require('apollo-server');
 
-const server = new ApolloServer();
-server.listen().then(({url}) => console.log(`server is running on ${url}`))
+const typesDefs = gql`
+  type Query {
+    sessions: [Session]
+  }
+  type Session {
+    id: ID!
+    title: String
+    description: String
+    startsAt: String
+    endsAT: String
+    room: String
+    day: String
+    format: String
+    track: String
+    level: String
+  }`;
+
+const server = new ApolloServer({typeDefs: typesDefs});
+server.listen({port: process.env.PORT || 4000}).then(({url}) => console.log(`server is running on ${url}`))
